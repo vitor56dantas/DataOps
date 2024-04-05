b52461128c20ebde6d81d6f447ef5614e4797c7e
Carolina <caroll.zambelli@gmail.com>
2024-04-04 21:30:47 -0300

    reestruturando em branches

--- aula01/ingestion.py
+++
@@ -1,9 +0,0 @@
-import requests
-import pandas as pd
-
-
-api_url = 'https://randomuser.me/api/?results=5'
-response = requests.get(api_url).json()
-data = response['results']
-df = pd.json_normalize(data)
-df.to_csv("raw.csv", sep=";", index=False)
